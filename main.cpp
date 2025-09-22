afdd052e690f34af1d382b39ad96dd7c4f51db16
DBCKS97 <wonyu1997@naver.com>
2025-09-22 11:09:35 +0900

    피라미드 별 찍기

--- main.cpp
+++ main.cpp
@@ -3,18 +3,23 @@
 using namespace std;
 
 
-//*****
-// ****
-//  ***
-//   **
-//    *
+
+//********** 10
+// ********   8
+//  ******    6
+//   ****     4
+//    **      2
 int main()
 {
 	int Number[10] = { 0 };
 
-	for (int i = 1; i <= 5; i++)
+	int Count = 0;
+	cin >> Count;
+	
+
+	for (int i = 1; i <= Count; i++)
 	{
-		for (int j = 1; j <= 5; j++)
+		for (int j = 1; j <= 2 * Count - (i - 1); j++)
 		{	
 			if (i > j)
 			{
