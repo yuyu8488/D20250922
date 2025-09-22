12671a325f226a18a1a54532e56c0332e27114f5
DBCKS97 <wonyu1997@naver.com>
2025-09-22 09:10:05 +0900

    Total 3의 배수

--- D20250922.cpp
+++ D20250922.cpp
@@ -9,8 +9,18 @@ int main()
 	{
 		Total += i;
 	}
+
+	int Total3 = 0;
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			Total3 += i;
+		}
+	}
 	
 	std::cout << "Total : " << Total << std::endl;
+	std::cout << "Total (3ÀÇ¹è¼ö) : " << Total3 << std::endl;
 
 	return 0;
 }
