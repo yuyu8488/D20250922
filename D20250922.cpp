56ee59b976b4d6bd1b801660ff0025dfdbafcfa5
DBCKS97 <wonyu1997@naver.com>
2025-09-22 09:04:42 +0900

    Initial commit

---
+++ D20250922.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+
+int main()
+{
+	int Total = 0;
+	for (int i = 1; i <= 100; i++)
+	{
+		Total += i;
+	}
+	
+	std::cout << "Total : " << Total << std::endl;
+
+	return 0;
+}
+
+
