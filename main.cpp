0a59aaf3c75f557485e489aafbf635a936df4f9f
DBCKS97 <wonyu1997@naver.com>
2025-09-22 16:45:08 +0900

    벽 이동 제한 처리

--- main.cpp
+++ main.cpp
@@ -1,36 +1,143 @@
-#include <iostream>
+// preprocessor: #include 
+#include <iostream> // cpp standard
+#include <conio.h>  // c   standard
+#include <vector> // container 
+#include <algorithm>
+#include <string>
+
 
 using namespace std;
 
+//**********
+//*        *
+//*        *
+//*        *
+//*        *
+//*        *
+//*        *
+//*        *
+//*        *
+//**********
 
-
-//********** 10
-// ********   8
-//  ******    6
-//   ****     4
-//    **      2
-int main()
+int World[10][10] =
 {
-	int Number[10] = { 0 };
+	{ 1,1,1,1,1,1,1,1,1,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,1,1,1,1,1,1,1,1,1 }
+};
 
-	int Count = 0;
-	cin >> Count;
-	
+int PlayerX = 1;
+int PlayerY = 1;
+char PlayerShape = 'P';
 
-	for (int i = 1; i <= Count; i++)
+// 사상(Mapping) -> 그림으로 바꿀예정.
+char Sprites[10] = { ' ', '*' };
+int KeyCode = 0;
+
+bool bIsRunning = true;
+
+void Input()
+{
+	// Input
+	KeyCode = _getch();
+
+
+}
+
+void Process()
+{
+	// Process
+	if (KeyCode == 'w')
+	{ 
+		if (World[PlayerY - 1][PlayerX] == 1)
+		{
+			return;
+		}
+		else
+		{
+			PlayerY--;
+		}
+	}
+	else if (KeyCode == 's')
 	{
-		for (int j = 1; j <= 2 * Count - (i - 1); j++)
-		{	
-			if (i > j)
+		if (World[PlayerY + 1][PlayerX] == 1)
+		{
+			return;
+		}
+		else
+		{
+			PlayerY++;
+		}
+	}
+	else if (KeyCode == 'a')
+	{
+		if (World[PlayerY][PlayerX - 1] == 1)
+		{
+			return;
+		}
+		else
+		{
+			PlayerX--;
+		}
+	}
+	else if (KeyCode == 'd')
+	{
+		if (World[PlayerY][PlayerX + 1] == 1)
+		{
+			return;
+		}
+		else
+		{
+			PlayerX++;
+		}
+	}
+	else if (KeyCode == 'q')
+	{
+		bIsRunning = false;
+	}
+
+
+}
+
+void Render()
+{
+	// 화면에 출력 --> 누가? OS -> BIOS -> firmware
+	// Render
+	system("cls");
+	for (int Y = 0; Y < 10; Y++)
+	{
+		for (int X = 0; X < 10; X++)
+		{
+			if (PlayerX == X && PlayerY == Y)
 			{
-				cout << " ";
+				cout << PlayerShape;
 			}
 			else
 			{
-				cout << "*";
+				cout << Sprites[World[Y][X]];
 			}
 		}
 		cout << "\n";
+	}
+}
+
+int main()
+{
+	// Frame, DeltaSeconds
+	while (bIsRunning)
+	{
+		Input();
+	
+		Process();
+	
+		Render();		
 	}
 
 	return 0;
